229ea3bc7924e02a92091acdf1617901093c9be0
umut-korkmaz <umutkorkmaz@gmail.com>
2024-11-09 16:31:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . 'vendor/autoload.php';
+require __DIR__ . './vendor/autoload.php';
 
 use ApplicationInsights\Telemetry_Client;
 use ApplicationInsights\Channel\Telemetry_Channel;
