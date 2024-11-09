a9e9c66bbb1cdfd1eceddb4dc70fb08e7e5400bf
umut-korkmaz <umutkorkmaz@gmail.com>
2024-11-09 15:42:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 echo "Hello World4!";
 error_log('test umut error log');
+
+$telemetryClient = new \ApplicationInsights\Telemetry_Client();
+$context = $telemetryClient->getContext();
+
+// Necessary
+$context->setInstrumentationKey('YOUR INSTRUMENTATION KEY');
+
+// Optional
+$context->getSessionContext()->setId(session_id());
+$context->getUserContext()->setId('YOUR USER ID');
+$context->getApplicationContext()->setVer('YOUR VERSION');
+$context->getLocationContext()->setIp('YOUR IP');
+
+// Start tracking
+$telemetryClient->trackEvent('name of your event');
+$telemetryClient->flush();
+
 ?>
